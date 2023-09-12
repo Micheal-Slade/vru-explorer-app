32ee643f16320f09ebdb206a2884c084581925e1
Micheal Slade <50627681+Micheal-Slade@users.noreply.github.com>
2023-09-12 11:51:00 +0100

    Updating project structure and testing index.js

--- app/main.R
+++ app/main.R
@@ -1,7 +1,7 @@
 # main.R
 
 box::use(
-  shiny[fluidPage, tags, uiOutput, moduleServer, NS]
+  shiny[fluidPage, tags, uiOutput, moduleServer, NS, reactive]
 )
 
 #' @export
@@ -9,15 +9,18 @@ ui <- function(id) {
   ns <- NS(id)
   fluidPage(
     tags$head(
-      tags$script(src = "static/lib/d3.v7.min.js"),
-      tags$script(src = "static/js/bar-chart.js")
+      tags$script(src = "static/d3.v7.min.js")
     ),
-    tags$svg(width = 400, height = 300, id = "d3_svg")
-  )
+    tags$div(id = ns("bar_chart"), style = "width: 400px; height: 300px;"),
+    tags$button(onclick = "App.drawBarChart()", "Draw Bar Chart"))
 }
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    # is server logic always needed?
+    ns <- session$ns
+
+    # TODO: data should be maipulated here then passed to JS
+    data <- data.frame(name = c("A","B","C"), value = c(23, 45, 67))
+     session$sendCustomMessage(type = 'prepareDataForChart', message = data)
   })
 }
